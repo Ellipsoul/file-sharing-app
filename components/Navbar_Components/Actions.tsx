86417a7aded02bf273a53f66c5deb17499f63821
Aron Teh <aronteh.chess@gmail.com>
2022-01-18 17:18:17 +0000

    Organised Navbar somewhat

--- components/Navbar_Components/Actions.tsx
+++ components/Navbar_Components/Actions.tsx
@@ -14,15 +14,6 @@ import MenuIcon from "@mui/icons-material/Menu";
 import Divider from "@mui/material/Divider";
 
 export default function Actions(): ReactElement {
-  // Handle theme in top navigation bar
-  const { theme, setTheme } = useTheme();
-  const toggleTheme = () => {
-    setTheme(theme === "light" ? "dark" : "light");
-    closeMenu();
-  };
-
-  const loaded = useLoaded(); // Custom hook to check if the page is loaded
-
   // Anchors the menu so button click opens it
   const [anchorEl, setAnchorEl] = useState(null);
   const open = Boolean(anchorEl);
@@ -37,14 +28,23 @@ export default function Actions(): ReactElement {
     setAnchorEl(null);
   };
 
-  return (
-    <>
-      {/* Main app page */}
+
+  // Handle theme in top navigation bar
+  const { theme, setTheme } = useTheme();
+  const toggleTheme = () => {
+    setTheme(theme === "light" ? "dark" : "light");
+    closeMenu();
+  };
+
+  const loaded = useLoaded(); // Custom hook to check if the page is loaded
+
+  const AppPageButton = () => {
+    return (
       <Link href="/app">
         <Button
           className="
-          px-4 py-2 rounded-xl hidden tiny:flex flex-col
-          bg-slate-700 dark:bg-slate-50 hover:bg-slate-800 dark:hover:bg-slate-200"
+      px-4 py-2 rounded-xl hidden tiny:flex flex-col
+      bg-slate-700 dark:bg-slate-50 hover:bg-slate-800 dark:hover:bg-slate-200"
           variant="contained"
           color="primary">
           <FolderSharedRoundedIcon
@@ -56,11 +56,15 @@ export default function Actions(): ReactElement {
             fontSize="medium"
           />
           <span className="text-white dark:text-slate-800 hidden md:block text-lg">
-            App
+        App
           </span>
         </Button>
       </Link>
-      {/* Link to About Page */}
+    );
+  };
+
+  const AboutPageButton = () => {
+    return (
       <Link href="/about">
         <Button
           className="
@@ -81,15 +85,11 @@ export default function Actions(): ReactElement {
           </span>
         </Button>
       </Link>
+    );
+  };
 
-      {/* Separates the naviation linkes from the theme toggler */}
-      <Divider
-        orientation="vertical"
-        flexItem
-        variant="middle"
-        className="bg-slate-800 dark:bg-white hidden tiny:block" />
-
-      {/* Theme toggler */}
+  const ToggleThemeButton = () => {
+    return (
       <Button
         className="
           p-2 md:p-4 rounded-xl md:rounded-full hidden tiny:block
@@ -102,34 +102,55 @@ export default function Actions(): ReactElement {
           <LightModeRoundedIcon fontSize="large" /> :
           <DarkModeRoundedIcon className="text-slate-800" fontSize="large" />}
       </Button>
+    );
+  };
 
-      {/* Menu with actions for mobile screens */}
-      <Button
-        className="rounded-xl md:rounded-full tiny:hidden block"
-        variant="text"
-        size="large"
-        aria-controls={open ? "basic-menu" : undefined}
-        aria-expanded={open ? "true": undefined}
-        onClick={openMenu}>
-        <MenuIcon className="text-slate-800 dark:text-white"/>
-      </Button>
-      <Menu
-        id="basic-menu"
-        anchorEl={anchorEl}
-        open={open}
-        onClose={closeMenu}
-        MenuListProps={{
-          "aria-labelledby": "basic-button",
-        }}
-      >
-        <Link href="/app">
-          <MenuItem onClick={closeMenu}>App</MenuItem>
-        </Link>
-        <Link href="/about">
-          <MenuItem onClick={closeMenu}>About</MenuItem>
-        </Link>
-        <MenuItem onClick={toggleTheme}>Toggle Theme</MenuItem>
-      </Menu>
+  const ActionsMenu = () => {
+    return (
+      <>
+        <Button
+          className="rounded-xl md:rounded-full tiny:hidden block"
+          variant="text"
+          size="large"
+          aria-controls={open ? "basic-menu" : undefined}
+          aria-expanded={open ? "true": undefined}
+          onClick={openMenu}>
+          <MenuIcon className="text-slate-800 dark:text-white"/>
+        </Button>
+        <Menu
+          id="basic-menu"
+          anchorEl={anchorEl}
+          open={open}
+          onClose={closeMenu}
+          MenuListProps={{
+            "aria-labelledby": "basic-button",
+          }}
+        >
+          <Link href="/app">
+            <MenuItem onClick={closeMenu}>App</MenuItem>
+          </Link>
+          <Link href="/about">
+            <MenuItem onClick={closeMenu}>About</MenuItem>
+          </Link>
+          <MenuItem onClick={toggleTheme}>Toggle Theme</MenuItem>
+        </Menu>
+      </>
+    );
+  };
+
+  return (
+    <>
+      <AppPageButton />
+      <AboutPageButton />
+      <Divider
+        orientation="vertical"
+        flexItem
+        variant="middle"
+        className="bg-slate-800 dark:bg-white hidden tiny:block" />
+      <ToggleThemeButton />
+      <ActionsMenu />
     </>
   );
 }
+
+
