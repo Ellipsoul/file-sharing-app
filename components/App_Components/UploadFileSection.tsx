20826920f09caca6536e78edbc1a7b348b156323
Aron Teh <aronteh.chess@gmail.com>
2022-01-21 21:25:36 +0000

    Refactored large uploadfile section into a different component. Added action buttons for each file

---
+++ components/App_Components/UploadFileSection.tsx
@@ -0,0 +1,108 @@
+import InsertDriveFileRoundedIcon from "@mui/icons-material/InsertDriveFileRounded";
+import { FileUploader } from "react-drag-drop-files";
+import { Button } from "@mui/material";
+
+interface UploadFileSectionProps {
+  file: File | null;
+  setFile: (file: File | null) => void;
+  clearFile: () => void;
+  uploadFile: () => Promise<void>
+}
+
+// Left side for uploading a file
+export default function UploadFileSection({
+  file, setFile, clearFile, uploadFile,
+}: UploadFileSectionProps) {
+  // Runs whenever a file is uploaded
+  const handleFileDrop = (file: File) => setFile(file);
+
+  // Convert bytes number to a nicer string
+  function niceBytes(n: number): string {
+    const units = ["bytes", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB"];
+    let l = 0;
+    while (n >= 1024 && ++l) {
+      n = n/1024;
+    }
+    return (n.toFixed(n < 10 && l > 0 ? 1 : 0) + " " + units[l]);
+  }
+
+  return (
+    <section className="
+          bg-neutral-100 dark:bg-neutral-800 border-2 border-neutral-300 dark:border-neutral-600
+          flex flex-col justify-start items-stretch gap-y-2 md:gap-y-4 w-full md:w-9/20
+          p-2 md:p-4 drop-shadow-xl"
+    >
+      {/* Header */}
+      <header className="
+          font-heading text-3xl md:text-4xl text-black dark:text-white text-center">
+          Upload File
+      </header>
+      {/* Draggable zone to upload a file */}
+      <FileUploader
+        name="file"
+        label={file?.name || "No file selected"}
+        file={file}
+        handleChange={handleFileDrop}
+        classes={
+          `file-uploader ${file ?
+            "border-green-400 dark:border-green-700":
+            "border-zinc-300 dark:border-zinc-500" }`}
+        hoverTitle=" "
+      >
+        {/* Display file information if a file is uploaded, otherwise prompt upload */}
+        { file ?
+          <div className="
+              flex flex-col h-full justify-evenly items-center p-3 sm:p-6 md:p-12
+              text-lg md:text-xl lg:text-2xl text-center font-slogan font-light
+              text-black dark:text-white">
+            <span className="text-7xl">
+              <InsertDriveFileRoundedIcon fontSize="inherit" />
+            </span>
+            <div className="w-full break-all">{file.name}</div>
+            <div className="w-full">{`Size: ${niceBytes(file.size)}`}</div>
+          </div> :
+          <div className="
+            flex flex-col h-full justify-evenly items-center p-6 sm:p-12
+            text-2xl md:text-3xl lg:text-4xl text-center font-slogan font-light
+            text-black dark:text-white">
+            <div className="">Drag&apos;n&apos;Drop File Here</div>
+            <div className="">Max Size: 100MB</div>
+          </div>
+        }
+      </FileUploader>
+      {/* Clear and Upload File Buttons */}
+      <div className="flex flex-row justify-evenly items-center px-2 gap-4">
+        {/* Clear */}
+        <Button className="
+            h-16 md:h-24 px-4 py-2 rounded-xl flex flex-row grow
+          bg-red-600 hover:bg-red-500"
+        variant="contained"
+        onClick={clearFile}
+        disabled={!file}>
+          <span
+            className={ `${file ? "text-white" :
+              "text-neutral-200 dark:text-neutral-600"} text-2xl md:text-3xl block`}
+          >
+              Clear
+          </span>
+        </Button>
+        {/* Upload */}
+        <Button
+          className="
+              h-16 md:h-24 px-4 py-2 rounded-xl
+              flex flex-row grow-2 bg-green-500 hover:bg-green-400"
+          variant="contained"
+          disabled={!file}
+          onClick={uploadFile}
+        >
+          <span
+            className={`${file ? "text-white" :
+              "text-neutral-200 dark:text-neutral-600"} text-2xl md:text-3xl block`}
+          >
+              Upload
+          </span>
+        </Button>
+      </div>
+    </section>
+  );
+};
--- pages/app.tsx
+++ pages/app.tsx
@@ -6,9 +6,11 @@ import { getDownloadURL, getMetadata, listAll, ref, StorageReference, uploadByte
   UploadMetadata, UploadTaskSnapshot } from "firebase/storage";
 import { signInWithPopup } from "firebase/auth";
 import { useAuthState } from "react-firebase-hooks/auth";
-import InsertDriveFileRoundedIcon from "@mui/icons-material/InsertDriveFileRounded";
-import { FileUploader } from "react-drag-drop-files";
+import IosShareRoundedIcon from "@mui/icons-material/IosShareRounded";
+import DownloadRoundedIcon from "@mui/icons-material/DownloadRounded";
+import DeleteRoundedIcon from "@mui/icons-material/DeleteRounded";
 
+import UploadFileSection from "../components/App_Components/UploadFileSection";
 import { storage } from "../lib/firebase";
 
 interface FileInfo {
@@ -21,12 +23,10 @@ export default function App(): ReactElement {
   // Tracking user authentication state
   const [user, loading, error] = useAuthState(auth);
 
-  // Tracking uploaded file
-  const [file, setFile] = useState<File | null>(null);
-  // Runs whenever a file is uploaded
-  const handleFileDrop = (file: File) => setFile(file);
   // Empties the file state
   const clearFile = () => setFile(null);
+  // Tracking uploaded file
+  const [file, setFile] = useState<File | null>(null);
 
   const [uploadingFile, setUploadingFile] = useState(false);
   const [retrievingFiles, setRetrievingFiles] = useState(false);
@@ -111,16 +111,6 @@ export default function App(): ReactElement {
     );
   };
 
-  // Convert bytes number to a nicer string
-  function niceBytes(n: number): string {
-    const units = ["bytes", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB"];
-    let l = 0;
-    while (n >= 1024 && ++l) {
-      n = n/1024;
-    }
-    return (n.toFixed(n < 10 && l > 0 ? 1 : 0) + " " + units[l]);
-  }
-
   // Button for Google Sign In
   function GoogleSignInButton(): ReactElement {
     const signInWithGoogle = async () => {
@@ -171,86 +161,41 @@ export default function App(): ReactElement {
     );
   }
 
-  // Left side for uploading a file
-  const UploadFileSection = () => {
+  interface FileActionProps {
+    file: FileInfo;
+  }
+
+  const FileActions = ({file}: FileActionProps) => {
     return (
-      <section className="
-        bg-neutral-100 dark:bg-neutral-800 border-2 border-neutral-300 dark:border-neutral-600
-        flex flex-col justify-start items-stretch gap-y-2 md:gap-y-4 w-full md:w-9/20
-        p-2 md:p-4 drop-shadow-xl"
-      >
-        {/* Header */}
-        <header className="
-        font-heading text-3xl md:text-4xl text-black dark:text-white text-center">
-        Upload File
-        </header>
-        {/* Draggable zone to upload a file */}
-        <FileUploader
-          name="file"
-          label={file?.name || "No file selected"}
-          file={file}
-          handleChange={handleFileDrop}
-          classes={
-            `file-uploader ${file ?
-              "border-green-400 dark:border-green-700":
-              "border-zinc-300 dark:border-zinc-500" }`}
-          hoverTitle=" "
-        >
-          {/* Display file information if a file is uploaded, otherwise prompt upload */}
-          { file ?
-            <div className="
-            flex flex-col h-full justify-evenly items-center p-3 sm:p-6 md:p-12
-            text-lg md:text-xl lg:text-2xl text-center font-slogan font-light
-            text-black dark:text-white">
-              <span className="text-7xl">
-                <InsertDriveFileRoundedIcon fontSize="inherit" />
-              </span>
-              <div className="w-full">{file.name}</div>
-              <div className="">{`Size: ${niceBytes(file.size)}`}</div>
-            </div> :
-            <div className="
-          flex flex-col h-full justify-evenly items-center p-6 sm:p-12
-          text-2xl md:text-3xl lg:text-4xl text-center font-slogan font-light
-          text-black dark:text-white">
-              <div className="">Drag&apos;n&apos;Drop File Here</div>
-              <div className="">Max Size: 100MB</div>
-            </div>
-          }
-        </FileUploader>
-        {/* Clear and Upload File Buttons */}
-        <div className="flex flex-row justify-evenly items-center px-2 gap-4">
-          {/* Clear */}
+      <div className="
+        flex flex-col justify-start items-center gap-y-1 rounded-lg w-full
+        p-3 bg-zinc-200 dark:bg-slate-600 font-heading break-all
+      ">
+        <div className="text-md">{file.name}</div>
+        <div className="flex flex-row w-full p-1 gap-x-2 justify-between">
           <Button className="
-          h-16 md:h-24 px-4 py-2 rounded-xl flex flex-row grow
-        bg-red-600 hover:bg-red-500"
-          variant="contained"
-          onClick={clearFile}
-          disabled={!file}>
-            <span
-              className={ `${file ? "text-white" :
-                "text-neutral-200 dark:text-neutral-600"} text-2xl md:text-3xl block`}
-            >
-            Clear
-            </span>
+            h-9 px-2 py-4 rounded-xl flex flex-row grow
+            bg-green-500 hover:bg-green-400"
+          variant="contained">
+            <span className="text-md hidden sm:block">Share</span>
+            <IosShareRoundedIcon fontSize="medium" className="block sm:hidden"/>
           </Button>
-          {/* Upload */}
-          <Button
-            className="
-            h-16 md:h-24 px-4 py-2 rounded-xl
-            flex flex-row grow-2 bg-green-500 hover:bg-green-400"
-            variant="contained"
-            disabled={!file}
-            onClick={uploadFile}
-          >
-            <span
-              className={`${file ? "text-white" :
-                "text-neutral-200 dark:text-neutral-600"} text-2xl md:text-3xl block`}
-            >
-            Upload
-            </span>
+          <Button className="
+            h-9 px-2 py-4 rounded-xl flex flex-row grow
+            bg-slate-700 dark:bg-slate-50 hover:bg-slate-800 dark:hover:bg-slate-200"
+          variant="contained">
+            <span className="text-md text-white dark:text-black hidden sm:block">Download</span>
+            <DownloadRoundedIcon fontSize="medium" className="block sm:hidden"/>
+          </Button>
+          <Button className="
+            h-9 px-2 py-4 rounded-xl flex flex-row grow
+          bg-red-600 hover:bg-red-500"
+          variant="contained">
+            <span className="text-md hidden sm:block">Delete</span>
+            <DeleteRoundedIcon fontSize="medium" className="block sm:hidden"/>
           </Button>
         </div>
-      </section>
+      </div>
     );
   };
 
@@ -264,14 +209,12 @@ export default function App(): ReactElement {
       >
         <div className="font-serif text-center text-2xl md:text-3xl">Uploaded Files</div>
         <div className="
-          flex flex-col grow min-h-48 max-h-96
-          border-2 border-zinc-300 rounded-lg p-2 overflow-y-scroll
+          flex flex-col grow min-h-48 max-h-96 gap-y-2
+          border-2 border-zinc-300 rounded-lg p-3 overflow-y-scroll
         ">
+          {/* Map the uploaded files to a file component */}
           {uploadedFiles.map((file, index) => (
-            <div key={index}>
-              <div>{file.name}</div>
-              <div>{file.downloadUrl}</div>
-            </div>
+            <FileActions file={file} key={index} />
           ))}
         </div>
         {user ? <SignOutButton /> : <GoogleSignInButton />}
@@ -286,7 +229,12 @@ export default function App(): ReactElement {
     <main className="
       grow flex flex-col md:flex-row justify-start md:justify-evenly items-stretch px-4 md:px-0 py-4
       flex-wrap gap-y-5">
-      <UploadFileSection />
+      <UploadFileSection
+        file={file}
+        setFile={setFile}
+        clearFile={clearFile}
+        uploadFile={uploadFile}
+      />
       <FileListSection />
     </main>
   );
