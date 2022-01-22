a7837a0494db8f1a1fc6ed679f6354c4ae99bd95
Aron Teh <aronteh.chess@gmail.com>
2022-01-22 16:28:57 +0000

    Trying another way to load up the images

--- pages/about.tsx
+++ pages/about.tsx
@@ -3,14 +3,36 @@ import Image from "next/image";
 
 export default function About(): ReactElement {
   return (
-    <main className='grow flex flex-row justify-evenly items-center flex-wrap gap-x-5 gap-y-5'>
-      <Image
-        src='/logo.png'
-        alt='TurboFile Logo'
-        quality={100}
-        width={200}
-        height={200}
-      />
+    <main className='
+      grow flex flex-col md:flex-row justify-between items-stretch gap-x-5 gap-y-5 md:px-10'>
+      <div className="about-containers">
+        <Image
+          src={"logo.png"}
+          alt='TurboFile Logo'
+          quality={100}
+          width={200}
+          height={200}
+        />
+      </div>
+      <div className="about-containers">
+        <Image
+          src={"logo.png"}
+          alt='TurboFile Logo'
+          quality={100}
+          width={200}
+          height={200}
+        />
+      </div>
+      <div className="about-containers">
+        <Image
+          src={"logo.png"}
+          alt='TurboFile Logo'
+          quality={100}
+          width={200}
+          height={200}
+        />
+      </div>
+
     </main>
   );
 }
