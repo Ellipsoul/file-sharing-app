3402f2817b9c391de846cd612cee32a1a15e1343
Aron Teh <aronteh.chess@gmail.com>
2022-01-22 14:31:57 +0000

    Trying a relative styling to see if positions get fixed

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,7 @@
 module.exports = {
   mode: "jit",
   darkMode: "class",
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
-    "./components/Navbar_Components/**/*.{js,ts,jsx,tsx}",
-    "./components/App_Components/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
       fontFamily: {
